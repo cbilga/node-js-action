07d8e82a8f1e0ed791e139bce45070591a2ef4a3
cbilga <cbilga@users.noreply.github.com>
2024-09-19 14:25:20 +0000

    Fix styling

--- test/sum.test.js
+++ test/sum.test.js
@@ -1,10 +1,10 @@
 //const sum = require("../sum");
-const {sum,min} = require("../sum")
+const { sum, min } = require("../sum");
 
 test("adds 1 + 2 to equal 3", () => {
-	expect(sum(1, 2)).toBe(3);
+  expect(sum(1, 2)).toBe(3);
 });
 
 test("adds 5 + 7 to equal 10", () => {
-	expect(sum(5, 7)).toBe(12);
+  expect(sum(5, 7)).toBe(12);
 });
